8f4481fbd62a737bc34a82f511ce46bc35b9112d
Jean-Marc R-Mahoro <jay-m@JMs-MacBook-Air.local>
2024-11-19 21:11:48 -0400

    Revert if not enough money sent for entrance fee

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -29,13 +29,22 @@ pragma solidity 0.8.19;
  * @dev Implements Chainlink VRFv2.5
  */
 contract Raffle {
+    /* Errors */
+    error Raffle__SendMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent!"); - Not gas effecient because string is being stored
+        // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle()); - only works on specific versions of Solidity with specific compiler version
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle(); //most gas efficient
+        }
+    }
 
     function pickWinner() public {}
 
