b3e8941cef3c1002cdb84b762a78184315ae5fb9
Jean-Marc R-Mahoro <jean_marcc@hotmail.com>
2024-11-22 00:15:22 -0400

    random words function enabled and build successfully

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -22,19 +22,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
 /**
  * @title Raffle contract
  * @author JM
  * @notice This contract is for creating a sample raffle
  * @dev Implements Chainlink VRFv2.5
  */
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2Plus {
     /* Errors */
     error Raffle__SendMoreToEnterRaffle();
 
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
     uint256 private immutable i_entranceFee;
-    // @dev the duration of the lottery in seconds
     uint256 private immutable i_interval;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
 
@@ -42,10 +49,20 @@ contract Raffle {
     event RaffleEntered(address indexed player); //Last change made
 
     // What data structure should we use? How to keep track of all players?
-    constructor(uint256 entranceFee, uint256 interval) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp; // starts the timer
+        i_keyHash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
@@ -58,13 +75,47 @@ contract Raffle {
         emit RaffleEntered(msg.sender);
     }
 
-    function pickWinner() external view {
+    function pickWinner() external {
         if ((block.timestamp - s_lastTimeStamp) < i_interval) {
             revert("The interval has not passed yet!");
         }
+        // requestId = s_vrfCoordinator.requestRandomWords(
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({
+            keyHash: i_keyHash,
+            subId: i_subscriptionId,
+            requestConfirmations: REQUEST_CONFIRMATIONS,
+            callbackGasLimit: i_callbackGasLimit,
+            numWords: NUM_WORDS,
+            extraArgs: VRFV2PlusClient._argsToBytes(
+                // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+            )
+        });
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
+
+        /*
+        struct RandomWordsRequest {
+    bytes32 keyHash;
+    uint256 subId;
+    uint16 requestConfirmations;
+    uint32 callbackGasLimit;
+    uint32 numWords;
+    bytes extraArgs;
+    }  
+        */
     }
 
-    /** Getter Functions */
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {
+        // uint256 winnerIndex = randomness % s_players.length;
+        // address payable winner = s_players[winnerIndex];
+        // winner.transfer(address(this).balance);
+        // s_players = new address payable[](0);
+        // s_lastTimeStamp = block.timestamp;
+    }
+
+    /**
+     * Getter Functions
+     */
     function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
