86d69ac2e4772a33bb44179f60b1e1ebb82297a8
Jean-Marc R-Mahoro <jean_marcc@hotmail.com>
2024-11-26 21:51:00 -0400

    code now compiles - fixed some bugs from yesteterday

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -5,6 +5,12 @@ import {Script} from "forge-std/Script.sol";
 import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
 abstract contract codeConstants {
+    /* VRG Mock Values */
+    uint96 public MOCK_BASE_FEE = 0.25 ether;
+    uint96 public MOCK_GAS_PRICE = 1e9;
+    // Link / ETH price
+    int256 public MOCK_WEI_PER_UINT_LINK = 4e15;
+
     uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
 }
@@ -29,10 +35,10 @@ contract HelperConfig is codeConstants, Script {
     }
 
     function getConfigByChainID(
-        uint256 chainID
+        uint256 chainId
     ) public returns (NetworkConfig memory) {
         if (networkConfigs[chainId].vrfCoordinator != address(0)) {
-            return networkConfigs[chainID];
+            return networkConfigs[chainId];
         } else if (chainId == LOCAL_CHAIN_ID) {
             // return localNetworkConfig;
         } else {
@@ -60,7 +66,21 @@ contract HelperConfig is codeConstants, Script {
 
         // Deploy mocks and such
         vm.startBroadcast();
-        VRFCoordinatorV2_5Mock vrfCoordinatorMock = new VRFCoordinatorV2_5Mock();
+        VRFCoordinatorV2_5Mock vrfCoordinatorMock = new VRFCoordinatorV2_5Mock(
+            MOCK_BASE_FEE,
+            MOCK_GAS_PRICE,
+            MOCK_WEI_PER_UINT_LINK
+        );
         vm.stopBroadcast();
+
+        localNetworkConfig = NetworkConfig({
+            entranceFee: 0.01 ether, // 1e16
+            interval: 30 seconds,
+            vrfCoordinator: address(vrfCoordinatorMock),
+            // gasLane addressdoesn't matter
+            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+            subscriptionId: 0, // might have to fix this
+            callbackGasLimit: 500000 // 500,000 gas
+        });
     }
 }
