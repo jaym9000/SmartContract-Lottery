1d29287343efff52673257462a97c95fbd11c436
Jean-Marc R-Mahoro <jean_marcc@hotmail.com>
2024-12-03 23:44:59 -0400

    implemented fuzz testing

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -6,6 +6,7 @@ import {DeployRaffle} from "script/DeployRaffle.s.sol";
 import {Raffle} from "src/Raffle.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
 import {Vm} from "forge-std/Vm.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
 contract RaffleTest is Test {
     Raffle public raffle;
@@ -169,5 +170,16 @@ contract RaffleTest is Test {
         Raffle.RaffleState raffleState = raffle.getRaffleState();
         assert(uint256(requestId) > 0);
         assert(uint256(raffleState) == 1);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                          FULLFILLRANDOMWORDS
+    //////////////////////////////////////////////////////////////*/
+
+    function testFulfillrandomWordsCanOnlyBeCalledAfterPerformingUpkeep(uint256 randomRequestId) public raffleEntered {
+        //randomRequestId in parameters by function name can be used as fuzz test to i
+        // Arrange / Act / Assert
+        vm.expectRevert(VRFCoordinatorV2_5Mock.InvalidRequest.selector);
+        VRFCoordinatorV2_5Mock(vrfCoordinator).fulfillRandomWords(randomRequestId, address(raffle));
     }
 }
